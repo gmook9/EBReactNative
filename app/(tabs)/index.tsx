51b43a52b353b2c2fb5bc733e8c6a9ae71dc4478
gmook9 <gmook9@gmail.com>
2024-08-19 20:41:04 -0700

    changed home page background colors to gradient

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,10 +2,15 @@ import React from 'react';
 import { StyleSheet, Image } from 'react-native';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function HomeScreen() {
   return (
     <ThemedView style={styles.container}>
+      <LinearGradient
+        colors={['#FF69B4', '#8A2BE2']} // Pink to Blue gradient
+        style={styles.background}
+      />
       <Image 
         source={require('../../assets/images/eb_butterfly.png')}
         style={styles.image} 
@@ -20,7 +25,9 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#0000FF',
+  },
+  background: {
+    ...StyleSheet.absoluteFillObject,
   },
   image: {
     width: 150, 
